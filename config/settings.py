236f891c8080f2d1bfbe3425334df527b0524b7f
quangnguyen3499 <baoquanggogreen@gmail.com>
2025-02-03 07:45:41 +0700

    fix: update staticfiles

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "app.chatbot",
+    "app.chatbot",
 ]
 
 MIDDLEWARE = [
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(os.path.abspath(BASE_DIR), "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
 ]
